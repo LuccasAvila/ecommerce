69e06611d6680e0aad023c5fbec73e0a0ba88369
Luccas Avila <luccasavila62@gmail.com>
2020-03-21 13:05:30 -0300

    Banner and cards

--- resources/views/store/home.blade.php
+++ resources/views/store/home.blade.php
@@ -13,5 +13,49 @@
             </li>
         </ul>
     </aside>
+    <div class="banner">
+        <img class="banner__item" src="https://via.placeholder.com/840x400" alt="Title">
+    </div>
 </header>
+
+<section class="cards">
+    <ul class="cards__list container">
+       <li class="cards__item">
+            <div class="cards__icon">
+                <span class="fas fa-shipping-fast"></span>
+            </div>
+            <div class="cards__info">
+                <p class="cards__title">Lorem, ipsum dolor.</p>
+                <p class="cards__text">Lorem ipsum dolor sit amet consectetur.</p>
+            </div>
+       </li>
+       <li class="cards__item">
+            <div class="cards__icon">
+                <span class="fas fa-shipping-fast"></span>
+            </div>
+            <div class="cards__info">
+                <p class="cards__title">Lorem, ipsum dolor.</p>
+                <p class="cards__text">Lorem ipsum dolor sit amet consectetur.</p>
+            </div>
+        </li>
+        <li class="cards__item">
+            <div class="cards__icon">
+                <span class="fas fa-shipping-fast"></span>
+            </div>
+            <div class="cards__info">
+                <p class="cards__title">Lorem, ipsum dolor.</p>
+                <p class="cards__text">Lorem ipsum dolor sit amet consectetur.</p>
+            </div>
+        </li>
+        <li class="cards__item">
+            <div class="cards__icon">
+                <span class="fas fa-shipping-fast"></span>
+            </div>
+            <div class="cards__info">
+                <p class="cards__title">Lorem, ipsum dolor.</p>
+                <p class="cards__text">Lorem ipsum dolor sit amet consectetur.</p>
+            </div>
+        </li>
+    </ul>
+</section>
 @endsection
