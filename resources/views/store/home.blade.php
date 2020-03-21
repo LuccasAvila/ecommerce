2571e58ae468896d5147f847eb64c23b97e8294a
Luccas Avila <luccasavila62@gmail.com>
2020-03-21 12:50:39 -0300

    Categories aside on home

--- resources/views/store/home.blade.php
+++ resources/views/store/home.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.store')
 
 @section('content')
-Lorem, ipsum.
+<header class="header container">
+    <aside class="categories">
+        <h2 class="categories__title">Categorias</h2>
+        <ul class="categories__list">
+            <li class="categories__item">
+                <a href="#" class="categories__link">Lorem </a>
+            </li>
+            <li class="categories__item">
+                <a href="#" class="categories__link">ipsum </a>
+            </li>
+        </ul>
+    </aside>
+</header>
 @endsection
