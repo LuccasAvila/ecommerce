4b539e40ee1f44effe7b46f4457d17444c932214
Luccas Avila <luccasavila62@gmail.com>
2020-03-22 19:11:25 -0300

    admin layout and navbar

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title', '') | Área administrativa</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/css/admin.css">
+    <script defer src="/js/all.min.js"></script></head>
+<body>
+    <nav class="navbar">
+        <div class="navbar__content container">
+            <div class="navbar__logo">
+                <h1 class="navbar__logo-text">
+                    <span class="text--secondary">E</span>COMMERCE
+                </h1>
+            </div>
+            <ul class="navbar__links">
+                <li><a class="navbar__link" href="#">Pedidos</a></li>
+                <li><a class="navbar__link" href="#">Produtos</a></li>
+                <li><a class="navbar__link" href="#">Usuários</a></li>
+            </ul>
+        </div>
+    </nav>
+</body>
+</html>
