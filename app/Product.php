488e9122b8110db414c92426f9bb44366a65df1f
Luccas Avila <luccasavila62@gmail.com>
2020-03-23 17:36:21 -0300

    ProductPhotos class, migration and relationship created

--- app/Product.php
+++ app/Product.php
@@ -13,4 +13,8 @@ class Product extends Model
     public function categories() {
         return $this->belongsToMany(Category::class);
     }
+
+    public function photos() {
+        return $this->hasMany(ProductPhoto::class);
+    }
 }
