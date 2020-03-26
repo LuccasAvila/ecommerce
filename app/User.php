d0b8eef9f70ab27c5d2ef25ac83239abbc7f2f9f
Luccas Avila <luccasavila62@gmail.com>
2020-03-26 15:34:24 +0000

    UserOrder created with model and migration

--- app/User.php
+++ app/User.php
@@ -36,4 +36,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function orders() {
+        return hasMany(UserOrder::class);
+    }
 }
